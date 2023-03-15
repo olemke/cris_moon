622673989f2275b24bc14a74c3e75f12e6147013
Oliver Lemke <olemke@gmail.com>
2023-03-15 10:48:01 +0100

    Calculate radiance averages

--- process_cris.py
+++ process_cris.py
@@ -13,6 +13,28 @@ speed_of_light_cgs = speed_of_light * 1e2
 cris_fov_diameter = 0.963
 
 number_of_extra_scans = 3
+radiance_average_extend = 2
+
+
+def cris_wavenumbers():
+    # for i=1:717
+    # wn(i)=648.75+(i-1)*(1096.25-648.75)/716
+    # end
+    # for i=718:717+869
+    # wn(i)=1208.75+(i-718)*(1751.25-1208.75)/868
+    # end
+    # for i=717+870:717+869+637
+    # wn(i)=2153.75+(i-717-870)*(2551.25-2153.75)/637
+    # end
+    wn = np.zeros((717 + 869 + 637))
+    for i in np.arange(0, 717):
+        wn[i] = 648.75 + i * (1096.25 - 648.75) / 716
+    for i in np.arange(717, 717 + 869):
+        wn[i] = 1208.75 + (i - 717) * (1751.25 - 1208.75) / 868
+    for i in np.arange(717 + 869, 717 + 869 + 637):
+        wn[i] = 2153.75 + (i - 717 - 869) * (2551.25 - 2153.75) / 637
+
+    return wn
 
 
 def find_cris_files(basedir, dirfilter="*"):
@@ -50,6 +72,49 @@ def calculate_cris_radiances(ds, dia, scanid, f_o_v, f_o_r):
     for_average = (ds.Robs[scanid, 0, f_o_v, :] + ds.Robs[scanid, 1, f_o_v, :]) / 2.
     return for_average / speed_of_light_cgs * mjy_sterad * (
         cris_fov_diameter / (dia[scanid, f_o_r, f_o_v] * 180. / np.pi))**2
+
+
+def calc_radiance_average(wavelength, extend, radiances):
+    """Calculate average radiance over a given wavelength range."""
+    wavelengths = 10000/cris_wavenumbers()
+    wl_pos = np.abs(wavelengths - wavelength).argmin()
+    return radiances[wl_pos - extend:wl_pos + extend + 1].mean()
+
+
+def calculate_radiance_averages(moon_intrusions, f_o_r, f_o_v, scanid, radiances):
+    wl = 5.8
+    varname = f"Rad_{f_o_r}_{f_o_v}_{scanid}_average_{str(wl).replace('.','_')}"
+    rad_avg_5_8 = calc_radiance_average(
+        wl, radiance_average_extend, radiances)
+    moon_intrusions[varname] = rad_avg_5_8
+    moon_intrusions[varname].attrs[
+        'description'] = f"Radiance average ({2*radiance_average_extend+1} values) around {wl}um for FOR {f_o_r} FOV {f_o_v} ScanID {scanid}"
+
+    wl = 6.1
+    varname = f"Rad_{f_o_r}_{f_o_v}_{scanid}_average_{str(wl).replace('.','_')}"
+    rad_avg_6_1 = calc_radiance_average(
+        wl, radiance_average_extend, radiances)
+    moon_intrusions[varname] = rad_avg_6_1
+    moon_intrusions[varname].attrs[
+        'description'] = f"Radiance average ({2*radiance_average_extend+1} values) around {wl}um for FOR {f_o_r} FOV {f_o_v} ScanID {scanid}"
+
+    wl = 6.2
+    varname = f"Rad_{f_o_r}_{f_o_v}_{scanid}_average_{str(wl).replace('.','_')}"
+    rad_avg_6_2 = calc_radiance_average(
+        wl, radiance_average_extend, radiances)
+    moon_intrusions[varname] = rad_avg_6_2
+    moon_intrusions[varname].attrs[
+        'description'] = f"Radiance average ({2*radiance_average_extend+1} values) around {wl}um for FOR {f_o_r} FOV {f_o_v} ScanID {scanid}"
+
+    varname = f"Rad_{f_o_r}_{f_o_v}_{scanid}_ratio_5_8_to_6_1"
+    moon_intrusions[varname] = rad_avg_5_8 / rad_avg_6_1
+    moon_intrusions[varname].attrs[
+        'description'] = f"Radiance ratio between 5.8um and 6.1um for FOR {f_o_r} FOV {f_o_v} ScanID {scanid}"
+
+    varname = f"Rad_{f_o_r}_{f_o_v}_{scanid}_ratio_avg_5_8_6_2_to_6_1"
+    moon_intrusions[varname] = (rad_avg_5_8 + rad_avg_6_2) / 2. /  rad_avg_6_1
+    moon_intrusions[varname].attrs[
+        'description'] = f"Radiance ratio between average of 5.8um+6.2um and 6.1um for FOR {f_o_r} FOV {f_o_v} ScanID {scanid}"
 
 
 def find_moon_intrusions(crisfile, wavelen_id=99, threshold=20):
@@ -101,6 +166,8 @@ def find_moon_intrusions(crisfile, wavelen_id=99, threshold=20):
                     moon_intrusions[varname].attrs[
                         'description'] = f"Radiances for FOR {f_o_r} FOV {f_o_v} ScanID {scanid}"
 
+                    calculate_radiance_averages(moon_intrusions, f_o_r, f_o_v, scanid, radiances)
+
                 (rad_0_maxind if f_o_r == 0 else rad_1_maxind).append((f_o_v, maxind))
             else:
                 logging.debug(
@@ -120,6 +187,12 @@ def find_moon_intrusions(crisfile, wavelen_id=99, threshold=20):
 
 
 def main():
+    wl = 10000/cris_wavenumbers()
+    print(cris_wavenumbers())
+    print((np.abs(wl - 6.05)).argmin())
+    print((np.abs(wl - 6.15)).argmin())
+    print((np.abs(wl - 6.2)).argmin())
+    print((np.abs(wl - 5.8)).argmin())
     for crisfile in find_cris_files(basedir='./SDR_data_npp', dirfilter="*"):
         moon_intrusions = find_moon_intrusions(crisfile, wavelen_id=99, threshold=20)
         if moon_intrusions:
