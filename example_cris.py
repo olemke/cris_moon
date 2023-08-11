665c8884124366a52e166b38ce0d4cca5af93b7a
Oliver Lemke <olemke@gmail.com>
2023-08-11 08:29:46 +0200

    Don't fail if no intrusions found

--- example_cris.py
+++ example_cris.py
@@ -2,4 +2,7 @@ from process_cris import (find_cris_files, find_moon_intrusions)
 
 for crisfile in find_cris_files(basedir='./SDR_data_npp', dirfilter="*"):
     moon_intrusions = find_moon_intrusions(crisfile, wavelen_id=99, threshold=20)
-    moon_intrusions.to_netcdf(f"Rad_{crisfile.name}")
+    if moon_intrusions:
+        moon_intrusions.to_netcdf(f"Rad_{crisfile.name}")
+    else:
+        print(f"No moon intrusions found in {crisfile.name}")
